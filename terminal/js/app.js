4a72b6779dbf79d2378470939d0a935cfa52527c
fr4j4 <fgonz@outlook.com>
2017-07-24 00:18:25 +0000

    terminal

--- terminal/js/app.js
+++ terminal/js/app.js
@@ -3,6 +3,19 @@ function init(){
 
 }
 
+
 function start(){
 	
+}
+
+function showTab(tabId){
+	console.log("show ["+tabId+"]");
+	$(".content").css("display","none");
+	$(".tab").addClass("inactive");
+	
+	$("#tab_"+tabId).removeClass("inactive");
+	$("#tab_"+tabId).removeClass("active");
+	
+	$("#tab_"+tabId).addClass("active");
+	$("#"+tabId).css("display","block");
 }
